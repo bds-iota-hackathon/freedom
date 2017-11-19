e5d636ca66f841fddd9ceaf20723537c4df95211
Rafal Hofman <rafal@Daniels-MacBook-Pro.local>
2017-11-19 10:48:15 +0100

    UI Loading update

--- app/controllers/freedomPassApplication.js
+++ app/controllers/freedomPassApplication.js
@@ -4,16 +4,21 @@ angular.module('MyApp')
         //$scope.foo = 'bar';
 
         $scope.sendFreedomPassApplication = function() {
+
+            $scope.loading = true;
+
             FreedomPassApplication.send($scope.freedomPassApplication)
                 .then(function(response) {
                     $scope.messages = {
                         success: [response.data]
                     };
+                    $scope.loading = false;
                 })
                 .catch(function(response) {
                     $scope.messages = {
                         error: Array.isArray(response.data) ? response.data : [response.data]
                     };
+                    $scope.loading = false;
                 });
         };
     });
